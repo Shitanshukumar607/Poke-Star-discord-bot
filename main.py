86d755f817426de9526f740bb5e4649bfa742ed8
Shitanshukumar607 <147914822+Shitanshukumar607@users.noreply.github.com>
2024-04-22 12:27:18 +0000

    A few basic commands

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+import discord
+from discord.ext import commands
+import os
+my_secret = os.environ['BOT_TOKEN']
 
-print("hello world")
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+  print("bot is ready")
+
+
+
+
+@bot.command()
+async def test(ctx):
+    message = await ctx.send('test')
+    emoji = '\N{THUMBS UP SIGN}'
+    await message.add_reaction(emoji)
+
+
+@bot.command()
+async def ping(ctx):
+  await ctx.send("Pong!")
+
+
+bot.run(my_secret)
